50001d7594e8eb3822a272c5150a37b9ac57ab2c
kismer16 <kismer@posta.ws>
2025-05-11 23:14:13 +0200

    v1-ok

---
+++ append.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import glob
+import openpyxl
+
+# Az Excel fájlok elérési útja
+file_path = 'H:/dp_pdf/innpro/xls/*.xlsx'  # Cseréld le a megfelelő elérési útra
+
+# Az összes Excel fájl beolvasása
+all_files = glob.glob(file_path)
+
+# Lista a DataFrame-ek tárolására
+dataframes = []
+
+# Minden fájl beolvasása és hozzáadása a listához
+for file in all_files:
+    df = pd.read_excel(file)
+    dataframes.append(df)
+
+# Az összes DataFrame összefűzése
+combined_df = pd.concat(dataframes, ignore_index=True)
+
+# Az összefűzött DataFrame mentése egy új Excel fájlba
+combined_df.to_excel('H:/dp_pdf/innpro/xls/combined_file.xlsx', index=False)
